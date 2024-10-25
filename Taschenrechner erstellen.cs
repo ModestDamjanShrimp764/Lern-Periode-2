422ebb066af38155e98c4dd8411be59229a34998
ModestDamjanShrimp764 <D.Krstic.inf24@stud.bbbaden.ch>
2024-10-25 11:43:39 +0200

    Add files via upload

---
+++ Taschenrechner erstellen.cs
@@ -0,0 +1,9 @@
+﻿// Zahl 1 eingeben
+Console.WriteLine("Gebe deine Zahl ein");
+double zahl1 = Convert.ToDouble(Console.ReadLine());
+
+// Operator eingeben
+Console.WriteLine("Gebe den Operator +,-,/,*");
+string operatorSymbol = Console.ReadLine();
+
+
---
+++ taschenrechner programm unterlagen test.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Willkomme beim Taschenrechner von Damjan");
+Console.WriteLine("Gebe sie die erste Zahl ein");
+double zahl1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Geben sie den Operator +,-,*,/");
+string operatorsymbol = Console.ReadLine();
+
+Console.WriteLine("Geben sie die Zahl2 ein");
+double zahl2 = Convert.ToDouble(Console.ReadLine());
+double ergebnis = 0;
+if (operatorsymbol == "+")
+{
+    ergebnis = zahl1 + zahl2;
+}
+else if (operatorsymbol == "-")
+{
+    ergebnis = zahl1 - zahl2;
+}
+else if (operatorsymbol == "*")
+{
+    ergebnis = zahl1 * zahl2;
+}
+else if (operatorsymbol == "/")
+{
+    ergebnis = zahl1 / zahl2;
+}
+else
+{
+    Console.WriteLine("Ungültiger Operator");
+}
+Console.WriteLine("ergebnis:" + ergebnis);
+
